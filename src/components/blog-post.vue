5cbf422f1b010488a48d1181d769cda6b240a1be
loewe0202 <loewe0202@163.com>
2019-03-06 18:29:17 +0800

    updata

---
+++ src/components/blog-post.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="blog-container">
+    <h2 class="blog-title">{{ news.title }}</h2>
+    <div class="blog-desc" v-html="news.desc"></div>
+    <el-row class="tc mgt-15">
+      <el-col :span="6" :offset="4">
+        <el-button
+          type="primary"
+          plain
+          @click="$emit('addFont', { num: Math.random() })"
+          >增大字体</el-button
+        >
+      </el-col>
+      <el-col :span="4">
+        <input
+          class="el-input__inner tc"
+          placeholder="请输入字体大小"
+          :value="value"
+          @input="$emit('input', Number($event.target.value))"
+        />
+      </el-col>
+      <el-col :span="6">
+        <el-button
+          type="primary"
+          plain
+          @click="$emit('delFont', { num: Math.random() })"
+          >减小字体</el-button
+        >
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: false
+    },
+    news: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "assets/components/blog-post.scss";
+</style>
--- src/components/button-counter.vue
+++ src/components/button-counter.vue
@@ -1,16 +1,35 @@
 <template>
-  <button @click="count++">You clicked me {{ count }} times.</button>
+  <div>
+    <h3 class="tc">
+      <slot></slot>
+    </h3>
+    <button @click="count += Math.floor(Math.random() * 10)">
+      Star Number: {{ countFn }}
+    </button>
+  </div>
 </template>
 <script>
 export default {
   // props: ["count"],
   props: {
-    count: {
+    number: {
+      type: Number,
       default: 0
     }
   },
   data() {
-    return {};
-  }
+    return {
+      count: 0
+    };
+  },
+  computed: {
+    countFn() {
+      return this.number + this.count;
+    }
+  },
+  methods: {}
 };
 </script>
+<style lang="scss" scoped>
+@import "assets/components/button-counter.scss";
+</style>
